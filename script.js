0824fd983ce60d0d101afacba7bf15098b14489d
jaji <57304962+tarirojaji@users.noreply.github.com>
2020-12-02 19:01:58 +0000

    randomize chorebot door

--- script.js
+++ script.js
@@ -4,27 +4,56 @@ let doorImage1 = document.querySelector('#door1')
 let doorImage2 = document.querySelector('#door2')
 let doorImage3 = document.querySelector('#door3')
 
+
+//randomize chorebot
+let numClosedDoors = 3;
+
+let openDoor1 = ''
+let openDoor2 = ''
+let openDoor3 = ''
+
+const randomChoreDoorGenerator = () => {
+    const choreDoor = Math.floor(Math.random() * numClosedDoors);
+
+    if (choreDoor === 0) {
+        openDoor1 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+    } else if (choreDoor === 1) {
+        openDoor2 = botDoorPath;
+        openDoor3 = beachDoorPath;
+        openDoor1 = spaceDoorPath;
+    } else if (choreDoor === 3) {
+        openDoor3 = botDoorPath;
+        openDoor1 = beachDoorPath;
+        openDoor2 = spaceDoorPath;
+    }
+
+}
+
 //door images
-let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
-let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg'
-let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg'
+const botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
+const beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg'
+const spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg'
 
 
 
 
 //doors
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath;
+    doorImage1.src = openDoor1;
 
 }
 
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 
 }
 
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
 
 }
+
+randomChoreDoorGenerator()
 
