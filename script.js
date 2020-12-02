d21329830fda2013730616ed8defa82ee71c40cf
jaji <57304962+tarirojaji@users.noreply.github.com>
2020-12-02 17:33:39 +0000

    door images added

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-let doorImage1 = document.querySelector('#door1')
-let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
 
+//doors
+let doorImage1 = document.querySelector('#door1')
+let doorImage2 = document.querySelector('#door2')
+let doorImage3 = document.querySelector('#door3')
+
+//door images
+let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg'
+let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg'
+let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg'
+
+
+
+
+//doors
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
 
 }
+
+doorImage2.onclick = () => {
+    doorImage2.src = beachDoorPath;
+
+}
+
+doorImage3.onclick = () => {
+    doorImage3.src = spaceDoorPath;
+
+}
+
